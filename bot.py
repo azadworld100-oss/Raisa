909e1d090929f2b10e5f3ba8287946d21350a148
azadworld100-oss <azadworld100@gmail.com>
2026-09-08 23:16:55 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,8 +81,8 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(reply)
 
 def main():
+    print("Bot Started Successfully")
     app = Application.builder().token(TELEGRAM_TOKEN).build()
-    
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     
